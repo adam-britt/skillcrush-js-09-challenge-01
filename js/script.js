a3e1b96e0876d4c09b0dd077db63b2f6d71c690a
Adam Britt <adamrussellbritt@gmail.com>
2023-07-27 16:24:22 -0500

    final commit

--- js/script.js
+++ js/script.js
@@ -8,21 +8,38 @@ const guestInput = document.querySelector(".add-guest input");
 const guestList = document.querySelector(".guest-list");
 // span class for number of guests attending
 const guestCount = document.querySelector(".attendance");
-// alert when guest list is full (not yet visible)
+// alert when guest list is full (not yet visible)s
 const guestFull = document.querySelector(".alert");
 
 addGuestButton.addEventListener("click", function() {
     const guest = guestInput.value;
-    console.log(guest);
+    // console.log(guest);
 
     if(guest !== "") {
-        const listItem = document.createElement("li");
-        listItem.innerText = guest;
-        guestList.append(listItem);
+        addToList(guest);
+        updateGuestCount();
         clearInput();
     }
 });
 
 const clearInput = function() {
     guestInput.value = "";
+}
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+}
+
+const updateGuestCount = function() {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+
+    if (guests.length === 0) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("guestFull");
+    }
 }
